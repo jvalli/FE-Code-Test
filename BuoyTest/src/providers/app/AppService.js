20d977c1e1cf1f7093a387e4f34398fd0b94e019
Jerónimo Valli <jeronimo.valli@gmail.com>
2019-04-30 13:38:46 -0300

    axios implementation

--- BuoyTest/src/providers/app/AppService.js
+++ BuoyTest/src/providers/app/AppService.js
@@ -1,51 +1,34 @@
+const axios = require('axios');
 const baseUrl = "http://www.thecocktaildb.com/api/json/v1/1";
 
 export default class AppService {
   static getCocktails(successCallback, errorCallback) {
     const endpoint = `${baseUrl}/filter.php?g=Cocktail_glass`;
-    fetch(endpoint, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-    }).then((response) => response.json())
-    .then( async (responseJson) => {
-      successCallback(responseJson.drinks);
+    axios.get(endpoint)
+    .then(function (response) {
+      successCallback(response.data.drinks);
     })
-    .catch((error) => {
+    .catch(function (error) {
       errorCallback(error);
     });
   }
   static getCocktailDetails(cocktailId, successCallback, errorCallback) {
     const endpoint = `${baseUrl}/lookup.php?i=${cocktailId}`;
-    fetch(endpoint, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-    }).then((response) => response.json())
-    .then( async (responseJson) => {
-      successCallback(responseJson.drinks[0]);
+    axios.get(endpoint)
+    .then(function (response) {
+      successCallback(response.data.drinks[0]);
     })
-    .catch((error) => {
+    .catch(function (error) {
       errorCallback(error);
     });
   }
   static searchCocktails(text, successCallback, errorCallback) {
     const endpoint = `${baseUrl}/search.php?s=${text}`;
-    fetch(endpoint, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-    }).then((response) => response.json())
-    .then( async (responseJson) => {
-      successCallback(responseJson.drinks);
+    axios.get(endpoint)
+    .then(function (response) {
+      successCallback(response.data.drinks);
     })
-    .catch((error) => {
+    .catch(function (error) {
       errorCallback(error);
     });
   }
